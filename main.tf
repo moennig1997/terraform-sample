0aa09344d193cc2e3a7c5a20a639f3400d76e934
moennig1997 <tamura-y@terra.dti.ne.jp>
2018-09-29 14:13:26 +0000

    commit main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+    default = "ap-northeast-1"
+}
+ 
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.region}"
+}
+
+resource "aws_vpc" "myVPC" {
+    cidr_block = "10.1.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "false"
+    tags {
+      Name = "myVPC"
+    }
+}
+
+resource "aws_internet_gateway" "myGW" {
+    vpc_id = "${aws_vpc.myVPC.id}" # myVPCのid属性を参照
+}
